1a5ce9c61657f223417a73b2980cdf075561d449
Lucas Sena <lucasferreiracyrax@gmail.com>
2026-04-24 15:44:10 -0300

    Classe Tabuleiro e metodos - gerarCampos, associarVizinhos

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -1,5 +1,48 @@
 package br.com.cod3r.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
 
+	private int linhas;
+	private int colunas;
+	private int minas;
+	
+	private List<Campo> campos = new ArrayList<Campo>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		super();
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for(int i = 0; i < linhas; i++) {
+			for(int j = 0; j<colunas; j++) {
+				campos.add(new Campo(i, j));
+			}
+		}
+		
+	}
+	
+	private void associarVizinhos() {
+		for(Campo c1 : campos) {
+			for(Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+		
+	}
+	
+	private void sortearMinas() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
