9d5642bdc8c1e6f6a3e4681e7e002aa8145c983c
Lucas Sena <lucasferreiracyrax@gmail.com>
2026-04-27 12:28:03 -0300

    Tabuleiro-Metodo sortearMinas, objetivoAlcancado, reiniciar, toString + Teste classe Aplicacao

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -2,6 +2,7 @@ package br.com.cod3r.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 
@@ -20,6 +21,20 @@ public class Tabuleiro {
 		gerarCampos();
 		associarVizinhos();
 		sortearMinas();
+	}
+	
+	public void abrir(int linha, int coluna) {
+		campos.stream()
+		.filter(c -> c.getLinha() == linha && c.getColuna()== coluna)
+		.findFirst()
+		.ifPresent(c -> c.abrir());
+	}
+	
+	public void alternarMarcacao(int linha, int coluna) {
+		campos.stream()
+		.filter(c -> c.getLinha() == linha && c.getColuna()== coluna)
+		.findFirst()
+		.ifPresent(c -> c.alternarMarcacao());
 	}
 
 	private void gerarCampos() {
@@ -41,8 +56,39 @@ public class Tabuleiro {
 	}
 	
 	private void sortearMinas() {
-		// TODO Auto-generated method stub
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();//verifica se o campo ja esta minado
 		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();//filtra os campos ja minados
+			int aleatorio = (int)(Math.random()*campos.size());// gera um numero entre 0 e 1, multiplica pelo tamanho do campo
+			campos.get(aleatorio).minar();//mina o campo gerado
+		}while(minasArmadas<minas);	//enquanto for menor que a quantidade de minas 
 	}
 	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c-> c.reiniciar());
+		sortearMinas();
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		int i = 0; //indice do tabuleiro
+		
+		for(int l = 0; l<linhas; l++) {//percorre as linhas 
+			for(int c = 0; c<colunas ; c++) {//percorre as colunas
+				sb.append(" ");
+				sb.append(campos.get(i));//escreve o que é naquele campo referente ao indice sorteado
+				sb.append(" ");
+				i++;//incrementa ao indice 
+			}
+			sb.append("\n");
+		}
+		return sb.toString();	
+	}
 }
