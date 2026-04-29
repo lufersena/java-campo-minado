d771d0488f94adaaab85c07bd9982735961f8cd7
Lucas Sena <lucasferreiracyrax@gmail.com>
2026-04-29 12:32:05 -0300

    Classe TabuleiroConsole e metodos + SairException

---
+++ src/br/com/cod3r/cm/visao/TabuleiroConsole.java
@@ -0,0 +1,85 @@
+package br.com.cod3r.cm.visao;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import br.com.cod3r.cm.excecao.ExplosaoException;
+import br.com.cod3r.cm.excecao.SairException;
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+public class TabuleiroConsole {
+	
+	private Tabuleiro tabuleiro;
+	private Scanner entrada = new Scanner(System.in);
+	
+	public TabuleiroConsole(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		
+		executarJogo();
+	}
+
+	private void executarJogo() {
+		try {
+			boolean continuar = true;
+			cicloDoJogo();
+			
+			while(continuar) {
+				System.out.println("Outra partida? (S/n)");
+				String resposta = entrada.nextLine();
+				
+				if("n".equalsIgnoreCase(resposta)) {
+					continuar = false;
+				}
+				else {
+					tabuleiro.reiniciar();
+				}
+			}
+			
+		}catch(SairException e) {
+			System.out.println("TCHAAAUUU!!");
+		}finally {
+			entrada.close();
+		}
+		
+	}
+
+	private void cicloDoJogo() {
+		try {
+			
+			while(!tabuleiro.objetivoAlcancado()) {//enquanto o objetivo nao for alcançado o loop continua
+				System.out.println(tabuleiro);
+				
+				String digitado = capturarValorDigitado("Digite (x, y): ");
+				
+				Iterator<Integer> xy =Arrays.stream(digitado.split(",")) //pega o string digitado ex("1,2") e os separa por virgula ex("1", "2")
+						.map(e -> Integer.parseInt(e.trim())).iterator();// converte para int tirando os espacos ex "1" e "2"
+ 				
+				digitado = capturarValorDigitado("1 - Abrir ou 2 - (Des)Marcar: ");
+				
+				if("1".equals(digitado)) {
+					tabuleiro.abrir(xy.next(), xy.next());
+				}else if ("2".equals(digitado)) {
+					tabuleiro.alternarMarcacao(xy.next(), xy.next());
+				}
+			}
+			
+		System.out.println("Você ganhou!!!!! ");
+		}catch(ExplosaoException e) {
+			System.out.println("Você perdeu!!!! ");
+		}
+		
+	}
+	
+	private String capturarValorDigitado(String texto) {
+		System.out.print(texto);
+		String digitado = entrada.nextLine();
+		
+		if("sair".equalsIgnoreCase(digitado)) {
+			throw new SairException();
+		}
+		return digitado;
+	}
+	
+
+}
